9a8311194f9e63b90d4e4fb260e722f7bc27457d
luoyanxi <1210257684@qq.com>
2019-04-01 17:35:27 +0800

    inti

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+'use strict'
+const Path = require('path')
+
+function resolve(dir) {
+    return Path.join(__dirname, dir)
+} 
+
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            extensions: ['.js', '.vue', '.json'],
+            alias: {
+                '@': resolve('/src'),
+                'common': resolve('/src/common'),
+            }
+        }
+    },
+    //别名配置的另一个方法
+    // chainWebpack(config) {
+    //     config.resolve.alias
+    //         .set('@', resolve('/src'))
+    //         .set('common', resolve('/src/common'))
+    // }, 
+}
